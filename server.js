f15c3014f8f98f70042ebee8940ec904413874cb
Amanda Molde <amanda.molde@gmail.com>
2018-07-16 20:02:57 -0600

    Set up server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+require('./db/db');
+
+const usersController = require('./controllers/users');
+const photosController = require('./controllers/photos');
+
+app.get('/', (req, res) => {
+	res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+	console.log('app is listening on port 3000');
+});
